d7522bf84c6139c52980398e71cf4875dea05043
AudenGDavis <audengdavis@gmail.com>
2025-09-04 13:43:15 +0000

    test commit

---
+++ CS2100/Sandboxes/PythonRefresher/main.py
@@ -0,0 +1,11 @@
+### order of operations with strings
+# print("I'm so excited" + "!" * 3)
+
+### F-String Formating
+# cats: int = 4
+# print(f"there are {cats} cats in this room")
+
+name: str = input("enter your name: ")
+age: int = int(input("enter your age: "))
+
+print(f"Hello {name}, you will turn 100 in the year {2025 + 100-age}")
